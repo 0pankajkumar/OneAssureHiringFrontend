4b87355dff4f30ad09f2aa6b28ed76ca0759de49
Pankaj Kumar <805.bluebell@gmail.com>
2023-07-20 17:44:05 +0530

    form works with submit

---
+++ my-app/src/PremiumFom.js
@@ -0,0 +1,135 @@
+import "./App.css";
+import { useState } from "react";
+
+export default function PremiumFom() {
+  const [sumInsured, setSumInsured] = useState(300000);
+  const [tierID, setTierID] = useState(1);
+  const [tenure, setTenure] = useState(1);
+  const [ages, setAges] = useState([1]);
+  const [message, setMessage] = useState("");
+
+  let handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      let res = await fetch("https://httpbin.org/post", {
+        method: "POST",
+        body: JSON.stringify({
+          SumInsured: sumInsured,
+          TierID: tierID,
+          Tenure: tenure,
+          Ages: ages,
+        }),
+      });
+      let resJson = await res.json();
+      if (res.status === 200) {
+        setSumInsured("");
+        setTierID("");
+        setTenure("");
+        setAges([]);
+        setMessage("User created successfully");
+      } else {
+        setMessage("Some error occured");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  function handleAgeInput(i, e) {
+    let newAges = [...ages];
+    newAges[newAges.length - 1] = parseInt(e.target.value);
+    setAges(newAges);
+  }
+
+  function removeFormFields(i) {
+    let newFormValues = [...ages];
+    newFormValues.splice(i, 1);
+    setAges(newFormValues);
+  }
+
+  function addFormFields() {
+    setAges([...ages, 0]);
+  }
+
+  return (
+    <div className="App">
+      <form onSubmit={handleSubmit}>
+        <label>
+          Sum Insured :
+          <select
+            name="sumInsured"
+            defaultValue="300000"
+            onChange={(e) => setSumInsured(e.target.value)}
+          >
+            <option value="300000">300000</option>
+            <option value="400000">400000</option>
+            <option value="500000">500000</option>
+          </select>
+        </label>
+        <br />
+
+        <label>
+          City Tier :
+          <select
+            name="tierID"
+            defaultValue="1"
+            onChange={(e) => setTierID(e.target.value)}
+          >
+            <option value="1">1</option>
+            <option value="2">2</option>
+          </select>
+        </label>
+        <br />
+
+        <label>
+          Tenure :
+          <select
+            name="tenure"
+            defaultValue="1"
+            onChange={(e) => setTenure(e.target.value)}
+          >
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+          </select>
+        </label>
+        <br />
+
+        {ages.map((element, index) => (
+          <div className="form-inline" key={index}>
+            <label>Age</label>
+            <input
+              type="text"
+              name="age"
+              value={element || ""}
+              onChange={(e) => handleAgeInput(index, e)}
+            />
+            {index ? (
+              <button
+                type="button"
+                className="button remove"
+                onClick={() => removeFormFields(index)}
+              >
+                Remove Member
+              </button>
+            ) : null}
+          </div>
+        ))}
+
+        <button className="" type="button" onClick={() => addFormFields()}>
+          Add Member
+        </button>
+
+        <div className="button-section">
+          <br />
+          <br />
+          <button className="button submit" type="submit">
+            Calculate Premium
+          </button>
+        </div>
+
+        <div className="message">{message ? <p>{message}</p> : null}</div>
+      </form>
+    </div>
+  );
+}
